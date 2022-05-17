d50db20920cac2020736e46d15ca2639b853bf06
Xeldar <srd.al@iamgod.in>
2022-05-17 12:33:50 +0300

    Added player movement and collision keys

--- src/main.lua
+++ src/main.lua
@@ -1,25 +1,79 @@
 -----------------------------------------------------------------------------------
 
 function love.load()
-    wf = require "library/windfield"
+    _windField = require "library/windfield"
+    _world = _windField.newWorld(0, 800, false)
 
-    world = wf.newWorld(0, 100)
+    _colliderKeys = {}
+    _colliderKeys.platform = "Platform"
+    _colliderKeys.player = "Player"
+    _colliderKeys.danger = "Danger"
 
-    player = world:newRectangleCollider(360, 100, 80, 80)
-    platform = world:newRectangleCollider(250, 400, 300, 100)
-    platform:setType("static")
+    _world:addCollisionClass(_colliderKeys.platform)
+    _world:addCollisionClass(_colliderKeys.player --[[, {ignores = {"Platform"}}]])
+    _world:addCollisionClass(_colliderKeys.danger)
+
+    _player = _world:newRectangleCollider(360, 100, 80, 80, { collision_class = _colliderKeys.player })
+    _player:setFixedRotation(true)
+    _player.speed = 240
+    
+    _platform = _world:newRectangleCollider(250, 400, 300, 100, { collision_class = _colliderKeys.platform })
+    _platform:setType("static")
+
+    _dangerZone = _world:newRectangleCollider(0, 550, 800, 50, { collision_class = _colliderKeys.danger })
+    _dangerZone:setType("static")
+
+    _controls = {}
+    _controls.up = "w"
+    _controls.down = "s"
+    _controls.left = "a"
+    _controls.right = "d"
 end
 
 -----------------------------------------------------------------------------------
 
 function love.update(dt)
-    world:update(dt)
+    _world:update(dt)
+
+    if (_player.body) then
+        movePlayer(dt)
+        checkPlayerCollision()
+    end
 end
 
 -----------------------------------------------------------------------------------
 
 function love.draw()
-    world:draw()
+    _world:draw()
+end
+
+-----------------------------------------------------------------------------------
+
+function movePlayer(dt)
+    local playerX, playerY = _player:getPosition()
+        
+    if (love.keyboard.isDown(_controls.right)) then
+        _player:setX(playerX + _player.speed * dt)
+    end
+    if (love.keyboard.isDown(_controls.left)) then
+        _player:setX(playerX - _player.speed * dt)
+    end
+end
+
+-----------------------------------------------------------------------------------
+
+function checkPlayerCollision()
+    if _player:enter((_colliderKeys.danger)) then
+        _player:destroy()
+    end
+end
+
+-----------------------------------------------------------------------------------
+
+function love.keypressed(key)
+    if(key ==_controls.up) then
+        _player:applyLinearImpulse(0, -5000)
+    end
 end
 
 -----------------------------------------------------------------------------------
